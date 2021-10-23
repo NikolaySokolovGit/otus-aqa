49607a10058ef5ab833c6a472ee718cc4b844fc9
Nikolay Sokolov <sxkxlxv@gmail.com>
2021-10-23 16:25:37 +0300

    add dog-api tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,3 @@
 def pytest_addoption(parser):
     parser.addoption('--url', action='store', default='https://ya.ru/', help='request url')
-    parser.addoption('--status_code', action='store', default=200, help='expected response status code')
+    parser.addoption('--status_code', action='store', default='200', help='expected response status code')
