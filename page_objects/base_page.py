ce1378774975ceef0c84a691e311cb3c49adc91d
nsokolov <sxkxlxv@gmail.com>
2021-12-05 14:53:12 +0300

    move common attributes to BasePage

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -1,9 +1,20 @@
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 
 class BasePage:
+    CURRENCY_FORM = (By.CSS_SELECTOR, '#form-currency')
+    CART = (By.CSS_SELECTOR, '#cart-total')
+    MENU = (By.CSS_SELECTOR, '#menu')
+    MY_ACCOUNT = (By.CSS_SELECTOR, '.dropdown')
+    LOGIN = (By.LINK_TEXT, 'Login')
+    REGISTER = (By.LINK_TEXT, 'Register')
+    CATALOGUE_DROPDOWN = (By.CSS_SELECTOR, '#menu .dropdown-toggle')
+    SEE_ALL = (By.CSS_SELECTOR, '.see-all')
+    SEARCH_FIELD = (By.CSS_SELECTOR, '#search')
+
     def __init__(self, driver):
         self.driver = driver
 
@@ -13,3 +24,15 @@ class BasePage:
             wait.until(ec.presence_of_element_located(locator))
         except TimeoutException:
             raise AssertionError(f'Элемент с локатором {locator} не найден за {timeout} с.')
+
+    def go_to_register(self):
+        self.driver.find_element(*self.MY_ACCOUNT).click()
+        self.driver.find_element(*self.REGISTER).click()
+
+    def go_to_login(self):
+        self.driver.find_element(*self.MY_ACCOUNT).click()
+        self.driver.find_element(*self.LOGIN).click()
+
+    def go_to_catalogue(self, index):
+        self.driver.find_elements(*self.CATALOGUE_DROPDOWN)[index].click()
+        self.driver.find_element(*self.SEE_ALL).click()
--- page_objects/main_page.py
+++ page_objects/main_page.py
@@ -4,28 +4,7 @@ from page_objects.base_page import BasePage
 
 
 class MainPage(BasePage):
-    SEARCH_FIELD = (By.CSS_SELECTOR, '#search')
     PRODUCT_LAYOUT = (By.CSS_SELECTOR, '.product-layout')
-    CURRENCY_FORM = (By.CSS_SELECTOR, '#form-currency')
-    CART = (By.CSS_SELECTOR, '#cart-total')
-    MENU = (By.CSS_SELECTOR, '#menu')
-    MY_ACCOUNT = (By.CSS_SELECTOR, '.dropdown')
-    LOGIN = (By.LINK_TEXT, 'Login')
-    REGISTER = (By.LINK_TEXT, 'Register')
-    CATALOGUE_DROPDOWN = (By.CSS_SELECTOR, '#menu .dropdown-toggle')
-    SEE_ALL = (By.CSS_SELECTOR, '.see-all')
-
-    def go_to_register(self):
-        self.driver.find_element(*self.MY_ACCOUNT).click()
-        self.driver.find_element(*self.REGISTER).click()
-
-    def go_to_login(self):
-        self.driver.find_element(*self.MY_ACCOUNT).click()
-        self.driver.find_element(*self.LOGIN).click()
 
     def go_to_product(self, index):
         self.driver.find_elements(*self.PRODUCT_LAYOUT)[index].click()
-
-    def go_to_catalogue(self, index):
-        self.driver.find_elements(*self.CATALOGUE_DROPDOWN)[index].click()
-        self.driver.find_element(*self.SEE_ALL).click()
