9f392ce00f18fe4924f2e60a0cbc1ea9edc43b6a
nsokolov <sxkxlxv@gmail.com>
2021-12-19 11:35:08 +0300

    replace id 9999 to default in sql request

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ def fill_in_the_field(field_element, value):
     field_element.send_keys(value)
 
 
-def db_add_admin_user(user='username', password='admin_password', email='some@email.com', user_id=9999):
-    sql = f"INSERT INTO oc_user VALUES ({user_id}, 1, '{user}', MD5('{password}'), '', 'FirstName', 'LastName', " \
+def db_add_admin_user(user='username', password='admin_password', email='some@email.com'):
+    sql = f"INSERT INTO oc_user VALUES (default, 1, '{user}', MD5('{password}'), '', 'FirstName', 'LastName', " \
           f"'{email}', '', '', '0.0.0.0', 1 , CURDATE());"
     with MariaDB() as db:
         db.execute(sql)
