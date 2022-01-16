5b38c7ebb4a54e40d3af5daf47a5011d43be7425
nsokolov <sxkxlxv@gmail.com>
2022-01-16 10:18:28 +0300

    add selenoid run option

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,20 +7,29 @@ def pytest_addoption(parser):
     parser.addoption("--url", action="store", required=True)
     parser.addoption("--admin_username", action="store", required=True)
     parser.addoption("--admin_password", action="store", required=True)
+    parser.addoption("--executor", action="store", default="192.168.0.102")
 
 
 @pytest.fixture
 def browser(request):
     driver = request.config.getoption('browser')
+    executor = request.config.getoption("--executor")
     url = request.config.getoption('url')
     url = f'{url}/' if not url.endswith('/') else url
 
-    drivers = {
-        'chrome': webdriver.Chrome,
-        'opera': webdriver.Opera,
-        'firefox': webdriver.Firefox,
-    }
-    driver = drivers[driver]()
+    if executor == 'local':
+        drivers = {
+            'chrome': webdriver.Chrome,
+            'opera': webdriver.Opera,
+            'firefox': webdriver.Firefox,
+        }
+        driver = drivers[driver]()
+    else:
+        caps = {
+            "browserName": driver
+        }
+        executor_url = f"http://{executor}:4444/wd/hub"
+        driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps)
 
     def finalizer():
         driver.quit()
