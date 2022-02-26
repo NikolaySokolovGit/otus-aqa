342d60862d26dcb403ff15e090a0cb142a88dea7
nsokolov <sxkxlxv@gmail.com>
2022-02-26 21:39:06 +0300

    add browser_ver option

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", choices=('chrome', 'opera', 'firefox'), default='chrome')
-    parser.addoption("--version", action="store", default=None)
+    parser.addoption("--browser_ver", action="store", default=None)
     parser.addoption("--url", action="store", required=True)
     parser.addoption("--admin_username", action="store", required=True)
     parser.addoption("--admin_password", action="store", required=True)
@@ -15,7 +15,7 @@ def pytest_addoption(parser):
 def browser(request):
     driver = request.config.getoption('browser')
     executor = request.config.getoption("executor")
-    version = request.config.getoption("version")
+    browser_ver = request.config.getoption("browser_ver")
     url = request.config.getoption('url')
     url = f'{url}/' if not url.endswith('/') else url
 
@@ -30,8 +30,8 @@ def browser(request):
         caps = {
             "browserName": driver,
         }
-        if version is not None:
-            caps["version"] = version
+        if browser_ver is not None:
+            caps["version"] = browser_ver
         executor_url = f"http://{executor}:4444/wd/hub"
         driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps)
 
